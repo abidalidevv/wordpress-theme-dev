7e3b581f0371dc941a82a171a73344b5f44f0e48
abidmmp <abidmmp99@gmail.com>
2018-08-20 10:11:00 +0500

    Add resource transformation

--- core_logic.php
+++ core_logic.php
@@ -29,3 +29,4 @@
 // Fix header injection guard - 2024-11-20 10:09:00
 // Fix route parameter binding - 2020-04-27 10:05:00
 // Improve form request - 2023-04-17 10:01:00
+// Add resource transformation - 2018-08-20 10:11:00
