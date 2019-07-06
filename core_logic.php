1eb93247cf10782e0ff7f49c0425dd5307ad93cc
abidmmp <abidmmp99@gmail.com>
2019-07-06 10:03:00 +0500

    Add request sanitization

--- core_logic.php
+++ core_logic.php
@@ -122,3 +122,4 @@
 // Fix route parameter binding - 2024-08-18 10:01:00
 // Refactor authentication logic - 2021-01-31 10:11:00
 // Improve query builder - 2020-08-01 10:07:00
+// Add request sanitization - 2019-07-06 10:03:00
