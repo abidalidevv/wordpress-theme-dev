d6d9ba08116b949f6293f30c931b7549a84ff8f4
abidmmp <abidmmp99@gmail.com>
2020-04-20 10:07:00 +0500

    Improve API pagination

--- core_logic.php
+++ core_logic.php
@@ -289,3 +289,4 @@
 // Improve dependency injection - 2021-10-02 10:05:00
 // Add request sanitization - 2017-06-07 10:01:00
 // Refactor helper functions - 2024-09-11 10:11:00
+// Improve API pagination - 2020-04-20 10:07:00
