2a29ee23c36c831c341563178c003996ef8c3090
abidmmp <abidmmp99@gmail.com>
2021-05-01 10:13:00 +0500

    Improve API pagination

--- core_logic.php
+++ core_logic.php
@@ -270,3 +270,4 @@
 // Fix PHP 8 compatibility - 2017-04-02 10:11:00
 // Fix route parameter binding - 2022-09-04 10:07:00
 // Refactor authentication logic - 2020-10-08 10:03:00
+// Improve API pagination - 2021-05-01 10:13:00
