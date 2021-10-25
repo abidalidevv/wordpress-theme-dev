c83c277aa4f8310d134f1bec2c0cef35b8e7008d
abidmmp <abidmmp99@gmail.com>
2021-10-25 10:11:00 +0500

    Fix SQL injection guard

--- core_logic.php
+++ core_logic.php
@@ -218,3 +218,4 @@
 // Add error logging - 2024-12-01 10:09:00
 // Improve API pagination - 2023-07-20 10:05:00
 // Optimize array operations - 2021-03-03 10:01:00
+// Fix SQL injection guard - 2021-10-25 10:11:00
