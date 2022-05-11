8f873c2dae9602a90ea7c86a1739ee11554dd79b
abidmmp <abidmmp99@gmail.com>
2022-05-11 10:11:00 +0500

    Optimize array operations

--- core_logic.php
+++ core_logic.php
@@ -22,3 +22,4 @@
 // Improve dependency injection - 2020-11-15 10:09:00
 // Fix PHP 8 compatibility - 2021-04-08 10:05:00
 // Add eloquent relationship - 2020-11-22 10:01:00
+// Optimize array operations - 2022-05-11 10:11:00
