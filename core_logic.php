f533246bd5fbcbd861bf70abb50d6626a07e6ba1
abidmmp <abidmmp99@gmail.com>
2023-11-08 10:05:00 +0500

    Fix route parameter binding

--- core_logic.php
+++ core_logic.php
@@ -139,3 +139,4 @@
 // Improve form request - 2017-10-05 10:03:00
 // Fix header injection guard - 2024-04-08 10:13:00
 // Improve file upload validation - 2020-07-09 10:09:00
+// Fix route parameter binding - 2023-11-08 10:05:00
