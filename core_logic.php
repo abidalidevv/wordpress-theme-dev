61db7e40a7f6279c20f90951541fcd1ad9fa65ad
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:03:00 +0500

    Add error logging

--- core_logic.php
+++ core_logic.php
@@ -3,3 +3,4 @@
 // Refactor controller method - 2017-05-06 10:01:00
 // Optimize array operations - 2024-11-11 10:11:00
 // Improve response formatting - 2018-06-12 10:07:00
+// Add error logging - 2024-03-07 10:03:00
