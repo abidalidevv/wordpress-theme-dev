5c341fb824d58322fddcfe94f430231785b4a368
Abid Ali <abidmmp99@gmail.com>
2025-08-19 17:27:32 +0000

    Improve form request

--- core_logic.php
+++ core_logic.php
@@ -354,3 +354,8 @@ function is_json(string $str): bool {
 function camel_to_snake(string $str): string {
     return strtolower(preg_replace('/[A-Z]/','_$0',lcfirst($str)));
 }
+
+<?php
+function slugify(string $text): string {
+    return preg_replace('/[^a-z0-9]+/', '-', strtolower(trim($text)));
+}
