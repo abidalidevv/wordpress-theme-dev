0f7ba752b453e24d1b96b74e63a55be54c2bf165
Abid Ali <abidmmp99@gmail.com>
2025-11-24 23:37:30 +0000

    Improve file upload validation

--- core_logic.php
+++ core_logic.php
@@ -337,3 +337,9 @@ function retry(callable $fn, int $times=3): mixed {
     while ($times--) { try { return $fn(); } catch(\Throwable $e) { $last=$e; } }
     throw $last;
 }
+
+<?php
+function is_json(string $str): bool {
+    json_decode($str);
+    return json_last_error() === JSON_ERROR_NONE;
+}
