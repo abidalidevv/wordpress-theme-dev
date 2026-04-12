afea4cc39a708d3a7b8d02add23241dd941a2f7c
Abid Ali <abidmmp99@gmail.com>
2026-04-12 12:06:45 +0000

    Improve file upload validation

--- core_logic.php
+++ core_logic.php
@@ -497,3 +497,8 @@ function human_bytes(int $bytes): string {
 function camel_to_snake(string $str): string {
     return strtolower(preg_replace('/[A-Z]/','_$0',lcfirst($str)));
 }
+
+<?php
+function slugify(string $text): string {
+    return preg_replace('/[^a-z0-9]+/', '-', strtolower(trim($text)));
+}
